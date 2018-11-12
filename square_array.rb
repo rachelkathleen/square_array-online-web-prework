52073aa047bf5aabf306fa08bfc9132a5abe6ab3
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-12 22:35:46 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
-def square_array(array)
-  squared = []
-    array.each {|element| squared << element ** 2}
-    return squared
-end
+#def square_array(array)
+  #squared = []
+    #array.each {|element| squared << element ** 2}
+    #return squared
+#end
 
-def square_array_collect(array)
+def square_array(array)
   array.collect do |element| element * element
   end
   array
